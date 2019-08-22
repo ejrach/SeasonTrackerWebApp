f340c146d944d58366db6f42859d4a910892f324
ejrach <eric.rach@gmail.com>
2019-08-22 15:49:54 -0500

    Initial commit

---
+++ UDP/Client/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace Host
+{
+    class Program
+    {
+        private static UdpClient client;
+        private static IPEndPoint receivePoint;
+        private static int myport = 6060;         //port for the client to use
+        private static int remote_port = 6767;  //port that server is using
+        private static int myip = 127001;         //ip address 127.0.0.1
+        private static string myhostname = "localhost";
+
+        static void initialize()
+        {
+            //create the UDP client
+            client = new UdpClient(myport);
+
+            //define a receive point
+            receivePoint = new IPEndPoint(new IPAddress(myip), myport);
+
+            //declare a thread
+            Thread startClient = new Thread(new ThreadStart(start_client));
+
+            //start the thread
+            startClient.Start();
+        }
+
+        static void start_client()
+        {
+            //loop flag
+            bool continueLoop = true;
+
+            while (continueLoop)
+            {
+                ASCIIEncoding encode = new ASCIIEncoding();
+
+                //Create the datagram
+                //Format: Hostname@Port@UniqueMessage
+                string sendString = myhostname + "@" + myport.ToString() + "@Give me Date Time";
+                //Encode it into byte array
+                byte[] sendData = encode.GetBytes(sendString);
+                Console.WriteLine();
+                Console.WriteLine("Asking for Date Time...");
+
+                //send to datagram to server, specified by it's port number
+                client.Send(sendData, sendData.Length, myhostname, remote_port);
+
+                //receive datagram from back from server
+                byte[] recData = client.Receive(ref receivePoint);
+                Console.WriteLine("Packet Received!!");
+                Console.WriteLine("Date Time received: {0}", encode.GetString(recData));
+
+                //close connection
+                client.Close();
+                Console.WriteLine("Connection Closed!!");
+
+                //end loop
+                continueLoop = false;
+            }
+
+            // wait for key input to close console
+            Console.Read();
+        }
+
+        static void Main(string[] args)
+        {
+            initialize();
+        }
+
+        
+    }
+}
---
+++ UDP/UDP/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Server
+{
+    class Program
+    {
+        private static UdpClient server;
+        private static IPEndPoint receivePoint;
+        private static int myport = 6767;            //port for the server to use
+        private static int ip = 127001;            //ip address 127.0.0.1
+
+        static void initalize()
+        {
+            Console.WriteLine("Server Initializing...");
+            //Create the UDP client
+            server = new UdpClient(myport);
+
+            //Define a receive point
+            receivePoint = new IPEndPoint(new IPAddress(ip), myport);
+
+            //declare a thread
+            Thread startServer = new Thread(new ThreadStart(start_server));
+
+            //start the thread
+            startServer.Start();
+        }
+
+        static void start_server()
+        {
+            Console.WriteLine("Server Started and waiting...");
+
+            //infinite Loop
+            //The server thread loops while waiting for a 
+            while (true)
+            {
+                //receive datagram
+                byte[] recData = server.Receive(ref receivePoint);
+                Console.WriteLine("Packet received from host!!");
+                ASCIIEncoding encode = new ASCIIEncoding();
+
+                //split it up
+                string[] temp = encode.GetString(recData).Split(new char[] { '@' });
+                Console.WriteLine();
+                Console.WriteLine("Received From: {0}", temp[0]);
+                Console.WriteLine("Port sent from: {0}", temp[1]);
+                Console.WriteLine("Content received: {0}", temp[2]);
+
+                //Send the response in a datagram
+                byte[] sendData = encode.GetBytes(DateTime.Now.ToString());
+                Console.WriteLine();
+                Console.WriteLine("Sending current Date Time to {0}...", temp[0]);
+
+                //We use the IP and port sent by the user to send the datagram back
+                //UDP (User Datagram Protocol) header format:
+                // 0            15 16           31
+                // -------------------------------
+                // | source port  |  dest port   |
+                // -------------------------------
+                // | UDP length   |  checksum    |
+                // -------------------------------
+                // UDP (array of bytes), length (num bytes), hostname, port
+                server.Send(sendData, sendData.Length, temp[0], Int32.Parse(temp[1]));
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            initalize();
+        }
+    }
+}
