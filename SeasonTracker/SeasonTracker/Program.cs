ae805d31ce1688efa1b71e57791f6299f4cb2358
ejrach <eric.rach@gmail.com>
2019-09-03 19:14:05 -0500

    initial commit

---
+++ SeasonTracker/SeasonTracker/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SeasonTracker
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            //TEMP: can be removed.
+            //var tvShow = new TvShow();
+            //tvShow.
+
+
+
+            Application.Run(new Home());
+        }
+    }
+}
---
+++ SeasonTracker/SeasonTracker/Record.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeasonTracker
+{
+    
+    public class Record
+    {
+        //Define the properties by typing "prop" then tab, and fill in the types and names.
+        //These properties are publicly acessible.
+        //Every Record has a...
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int EpisodeCount { get; set; }
+        public int SeasonNumber { get; set; }
+
+        //These properties are private. They don't need to be accessible to the public.
+        //Encapsulation of this data since the implementation and behavior should be hidden.
+        private string _watchlist;
+
+        public Record()
+        {
+
+        }
+
+
+    }
+}
