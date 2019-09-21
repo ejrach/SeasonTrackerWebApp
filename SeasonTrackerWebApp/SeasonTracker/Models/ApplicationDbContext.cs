4a844854d8a55aecb3e01f9780fc5d494be001ca
ejrach <eric.rach@gmail.com>
2019-09-21 11:07:03 -0500

    Split out ApplicationDbContext and ApplicationUser into their own classes

--- SeasonTrackerWebApp/SeasonTracker/Models/ApplicationDbContext.cs
+++ SeasonTrackerWebApp/SeasonTracker/Models/ApplicationDbContext.cs
@@ -1,27 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security.Claims;
-using System.Web;
-using System.Threading.Tasks;
-using System.Data.Entity;
-using Microsoft.AspNet.Identity;
+﻿using System.Data.Entity;
 using Microsoft.AspNet.Identity.EntityFramework;
 
 namespace SeasonTracker.Models
 {
-    // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
-    public class ApplicationUser : IdentityUser
-    {
-        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
-        {
-            // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
-            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
-            // Add custom user claims here
-            return userIdentity;
-        }
-    }
-
     //The IdentityDbContext is the gateway to our database. It is part of Asp.net identity framework.
     //So when we execute enable-migration, Entity Framework (EF) looked at our IdentityDbContext and it discovered
     //Db sets in IdentityDbContext like user, role, etc.
